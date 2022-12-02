818d92d585e83ee9b0f154070fe5615a59be135a
kittithatj <92021493+kittithatj@users.noreply.github.com>
2022-12-02 22:13:50 +0700

    initialize project

---
+++ src/main/java/com/pim/develize/DevelizeApplication.java
@@ -0,0 +1,15 @@
+package com.pim.develize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelizeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DevelizeApplication.class, args);
+        System.out.println("Develize Started!");
+    }
+
+}
---
+++ src/test/java/com/pim/develize/DevelizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pim.develize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
